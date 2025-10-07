c10b7d658c60e712fa3bd10ad91829b7e3c4018b
bodinggg <66398434+bodinggg@users.noreply.github.com>
2025-10-07 20:07:44 +0800

    Update app.py
    总体添加了log记录，添加终端获取本地模型路径

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
-from src.model import *
-from src.workflow import *
-from src.client import *
-from src.node import *
-from src.show import *
-from src.state import *
-from src.tool import *
+import argparse
+from src.workflow import create_workflow
+from src.show import print_save
+from src.log_config import loggers
+
+
+logger = loggers['main']
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--model-path',
+                        required=True,
+                        help="你的本地模型路径")
+    return parser.parse_args()
+
 
 def main():
-    model_path = "your-local-model-path"
+    args = get_parser()
+    model_path = args.model_path
     
     try:
+        logger.info("开始小说生成流程")
+        logger.info(f"使用模型路径：{model_path}")
+        
         app = create_workflow(model_path)
         user_intent = input("请输入你的小说创作意图：")
-
+        logger.info(f"用户创作意图: {user_intent}")
         result = app.invoke(
             {
                 "user_intent":user_intent,
@@ -21,6 +34,7 @@ def main():
                 "recursion_limit": 1000  # 限制最大循环次数
             }
         )
+        logger.info("小说生成流程完成, 准备输出结果")
         print_save(result)
         
     except Exception as e:
