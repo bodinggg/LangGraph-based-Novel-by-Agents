bfcd322a537f6e4c504fd876fd2c72e1a2a774fe
bodinggg <66398434+bodinggg@users.noreply.github.com>
2025-10-09 22:08:53 +0800

    Update config_loger.py

--- src/config_loger.py
+++ src/config_loger.py
@@ -2,24 +2,33 @@
 import yaml
 from pydantic import BaseModel
 from pathlib import Path
-from typing import Dict, Any
+from typing import Dict, Any, Optional
+
+class ModelConfig(BaseModel):
+    model_type: str = "api"
+    model_path: Optional[str] = None
+    model_name: Optional[str] = None
+    api_url: Optional[str] = None
+    api_key: Optional[str] = None
+    max_retries: int=3  # 重试次数，可选
+    retry_delay: int=1  # 等待延迟，可选
+
 
 class BaseConfig(BaseModel):
     max_new_tokens: int
     temperature: float
     top_p: float
 
-class WriterConfig(BaseConfig):
-    min_word_length: int
     
 class ConfigLoader:
     def __init__(self, config_path:str="config.yaml"):
         self.config_path = Path(config_path)
         self.config_data = self._load_config()
+        
        
         self.outline_config = BaseConfig(** self.config_data["outline_config"])
         self.character_config = BaseConfig(**self.config_data["character_config"])
-        self.writer_config = WriterConfig(** self.config_data["writer_config"])
+        self.writer_config = BaseConfig(** self.config_data["writer_config"])
         self.reflect_config = BaseConfig(**self.config_data["reflect_config"])
     
     def _load_config(self) -> Dict[str, Any]:
