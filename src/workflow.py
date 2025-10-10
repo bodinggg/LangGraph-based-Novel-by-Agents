106c7c6580c07d7c90a1ea9bafe9621e7ea6dcb1
bodinggg <66398434+bodinggg@users.noreply.github.com>
2025-10-11 01:28:08 +0800

    分卷逻辑QaQ

--- src/workflow.py
+++ src/workflow.py
@@ -1,6 +1,7 @@
 """
 定义工作状态, 核心部分
 """
+from typing import Dict
 from langgraph.graph import StateGraph, END
 from src.agent import OutlineGeneratorAgent, CharacterAgent, WriterAgent, ReflectAgent 
 from src.node import *
@@ -13,13 +14,14 @@ from src.config_loader import (
     CharacterConfig,
     WriterConfig,
     ReflectConfig,
+    BaseConfig,
     ModelConfig
 )
 
 logger = loggers['workflow']
 
 # 构建工作流
-def create_workflow(model_config: ModelConfig) -> StateGraph:
+def create_workflow(model_config: ModelConfig, Agent_config: BaseConfig= None) -> StateGraph:
     """创建包含章节写作和质量评审的完整工作流"""
     # 获取共享模型实例和分词器
     model_type = model_config.model_type
@@ -35,7 +37,13 @@ def create_workflow(model_config: ModelConfig) -> StateGraph:
             retry_delay=model_config.retry_delay
         )
     logger.info(f"成功加载{model_type}模型管理器")
-    
+    print(f"[test] OutlineConfig is {OutlineConfig}")
+    # 允许改变config值
+    if Agent_config is not None:
+        OutlineConfig.min_chapters = Agent_config.min_chapters
+        OutlineConfig.volume = Agent_config.volume
+        OutlineConfig.master_outline = Agent_config.master_outline
+    print(f"[test] OutlineConfig is {OutlineConfig}")
     # 初始化 Agent
     outline_agent = OutlineGeneratorAgent(model_manager, OutlineConfig)    # 大纲
     character_agent = CharacterAgent(model_manager, CharacterConfig)         # 角色
@@ -47,10 +55,27 @@ def create_workflow(model_config: ModelConfig) -> StateGraph:
     # 创建图
     workflow = StateGraph(NovelState)
     # -------------------- 创建节点 --------------------
-    # 大纲
-    workflow.add_node("generate_outline", 
-                     lambda state: generate_outline_node(state, outline_agent))
-    workflow.add_node("validate_outline", validate_outline_node)
+    print(f"[test] master_outline is {OutlineConfig.master_outline}")
+    if OutlineConfig.master_outline:
+        # 分卷
+        workflow.add_node("generate_master_outline",
+                        lambda state: generate_master_outline_node(state, outline_agent))
+        workflow.add_node("validate_master_outline", validate_master_outline_node)
+        
+        # 分章
+        workflow.add_node("generate_volume_outline", 
+                        lambda state: generate_volume_outline_node(state, outline_agent))
+        workflow.add_node("validate_volume_outline", validate_volume_outline_node)
+        
+        # 合并
+        workflow.add_node("accpet_outline", accept_outline_node)
+        workflow.add_node("volume2character", volume2character)
+    else:
+        # 大纲
+        workflow.add_node("generate_outline", 
+                        lambda state: generate_outline_node(state, outline_agent))
+        workflow.add_node("validate_outline", validate_outline_node)
+        
     
     # 角色
     workflow.add_node("generate_characters", 
@@ -86,18 +111,52 @@ def create_workflow(model_config: ModelConfig) -> StateGraph:
     
     
     # -------------------- 创建边 --------------------
-    # 大纲
-    workflow.set_entry_point("generate_outline")
-    workflow.add_edge("generate_outline", "validate_outline")
-    workflow.add_conditional_edges(
-        "validate_outline",
-        check_outline_node,
-        {
-            "success": "generate_characters",
-            "retry": "generate_outline",
-            "failure": "failure"
-        }
-    )
+    # 大纲(原逻辑保留)
+    
+    if OutlineConfig.master_outline:
+        workflow.set_entry_point("generate_master_outline")
+        workflow.add_edge("generate_master_outline", "validate_master_outline")
+        workflow.add_conditional_edges(
+            "validate_master_outline",
+            check_master_outline_node,
+            {
+                "success": "generate_volume_outline",
+                "retry": "generate_master_outline",
+                "failure": "failure"
+            }
+        )
+        workflow.add_edge("generate_volume_outline", "validate_volume_outline")
+        workflow.add_conditional_edges(
+            "validate_volume_outline",
+            check_volume_outline_node,
+            {
+                "success": "volume2character",
+                "retry": "generate_volume_outline",
+                "failure": "failure"
+            }
+        )
+        workflow.add_edge("volume2character", "accpet_outline")
+        workflow.add_conditional_edges(
+            "accpet_outline",
+            check_outline_completion_node,
+            {
+                "complete":"generate_characters",
+                "continue":"generate_volume_outline"
+            }
+        )
+        
+    else:
+        workflow.set_entry_point("generate_outline")
+        workflow.add_edge("generate_outline", "validate_outline")
+        workflow.add_conditional_edges(
+            "validate_outline",
+            check_outline_node,
+            {
+                "success": "generate_characters",
+                "retry": "generate_outline",
+                "failure": "failure"
+            }
+        )
     
     # 角色档案
     workflow.add_edge("generate_characters", "validate_characters")
