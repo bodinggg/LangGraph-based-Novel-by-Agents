db4951b359b1d819d782f7e26980f3f706128662
bodinggg <66398434+bodinggg@users.noreply.github.com>
2025-10-13 23:08:28 +0800

    Update state.py

--- src/state.py
+++ src/state.py
@@ -4,11 +4,16 @@
 
 from typing import Optional, List
 from src.model import * 
-    
+from src.storage import NovelStorage
+from pydantic import BaseModel, ConfigDict
+
 class NovelState(BaseModel):
+    # 动态内存管理
+    novel_storage: Optional[NovelStorage] = None
+    
     # 用户意图
     user_intent: str
-    
+
     # 重要参数控制
     min_chapters: int=10
     
@@ -21,21 +26,22 @@ class NovelState(BaseModel):
     
     # 大纲生成控制
     raw_outline: Optional[str]=None
-    validated_outline: Optional[NovelOutline]=None
+    validated_outline: Optional[NovelOutline]=None  # 分卷生成大纲需要用到，属于动态更新，先不拆成本地
     outline_validated_error: Optional[str]=None
+
+    
     # 分卷支持更多章节数大纲
     raw_master_outline: Optional[str]=None
     current_volume_index: int=0
     raw_volume_chapters: Optional[str]=None
     validated_chapters: List[ChapterOutline]=[]
     
+    
+    
     # 角色档案生成控制
     row_characters : Optional[str] = None
-    validated_characters: Optional[List[Character]]=None
     characters_validated_error: Optional[str] = None
 
-    # 全文存储
-    chapters_content: Optional[List[ChapterContent]] = []
     
     # 章节内容生成控制
     current_chapter_index: int=0
@@ -49,10 +55,7 @@ class NovelState(BaseModel):
     validated_evaluation : Optional[QualityEvaluation] = None
     evaluation_validated_error: Optional[str] = None
     
-    # 结果
-    result : Optional[str] = None
-    final_outline: Optional[NovelOutline]=None
-    final_characters: Optional[List[Character]]=None
-    final_content: Optional[List[ChapterContent]] = None
-    final_error: Optional[str] = None	
+    	
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+    
     
